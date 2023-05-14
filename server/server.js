5c234f33c6ded9cd2a75c8a6ad455d8993b849ce
Everett Butler <everettjamesbutler@gmail.com>
2023-05-14 16:56:48 -0500

    got database up and running and inserted sample data

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use('/api/shelf', shelfRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5008;
+const PORT = process.env.PORT || 5021;
 
 /** Listen * */
 app.listen(PORT, () => {
